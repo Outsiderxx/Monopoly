5aff27060660851e5a8cfd57b32f0c07af534eb6
iu0518 <zj4u6m45610@gmail.com>
2019-05-27 00:40:23 +0800

    修改股票模式

--- Monopoly/Monopoly/Bank.cpp
+++ Monopoly/Monopoly/Bank.cpp
@@ -15,6 +15,14 @@ Bank::Bank()
 		ownStock[i] = 0;
 		upDownLimit[i] = 0;
 	}
+	for (int i = 0; i < numOfStocks; ++i)
+	{
+		minStockPrice[i] = StockPrice[i] * 0.88;
+		maxStockPrice[i] = StockPrice[i] * 1.12;
+	}
+	lastTime = time(NULL);
+	nowTime = time(NULL);
+	flowTimes = 0;
 }
 Bank::~Bank() {}
 
@@ -23,36 +31,41 @@ void Bank::setLoan_times(int times) { loan_times = times; }
 void Bank::setInterest(int inter) { interest = inter; }
 void Bank::stockPriceFlow()
 {
-	roundCount++;
-	float range;
-	if (roundCount = 1)
+	nowTime = time(NULL);
+	if (nowTime - lastTime >= 10)
 	{
+		lastTime = nowTime;
+		flowTimes++;
+		float range;
 		for (int i = 0; i < numOfStocks; ++i)
 		{
-			minStockPrice[i] = StockPrice[i] * 0.88;
-			maxStockPrice[i] = StockPrice[i] * 1.12;
+			range = 0.95 + (float(rand()) / RAND_MAX) * 0.1; //每次波動幅度為正負5%(原股價 * (0.95~1.05))
+			if (StockPrice[i] * range >= maxStockPrice[i]) //漲停
+			{
+				StockPrice[i] = maxStockPrice[i];
+				upDownLimit[i] = 1;
+			}
+			else if (StockPrice[i] * range <= minStockPrice[i]) // 跌停
+			{
+				StockPrice[i] = minStockPrice[i];
+				upDownLimit[i] = 2;
+			}
+			else
+			{
+				StockPrice[i] *= range;
+				upDownLimit[i] = 0;
+			}
+		}
+		if (flowTimes == 6) // 每波動六次更新漲停標準
+		{
+			flowTimes = 0;
+			for (int i = 0; i < numOfStocks; ++i)
+			{
+				minStockPrice[i] = StockPrice[i] * 0.88;
+				maxStockPrice[i] = StockPrice[i] * 1.12;
+			}
 		}
 	}
-	for (int i = 0; i < numOfStocks; ++i)
-	{
-		range = (float(rand()) / RAND_MAX) * 0.05;
-		if (StockPrice[i] * range >= maxStockPrice[i]) //漲停
-		{
-			StockPrice[i] = maxStockPrice[i];
-			upDownLimit[i] = 1;
-		}
-		else if (StockPrice[i] * range <= minStockPrice[i]) // 跌停
-		{
-			StockPrice[i] = minStockPrice[i];
-			upDownLimit[i] = 2;
-		}
-		else
-		{
-			StockPrice[i] *= range;
-			upDownLimit[i] = 0;
-		}
-	}
-	if (roundCount == 4) roundCount = 0;
 }
 void Bank::setOwnStock(int StockId, int num) { ownStock[StockId] = num; }
 void Bank::saveMoney(int dep) // 存款
--- Monopoly/Monopoly/Bank.h
+++ Monopoly/Monopoly/Bank.h
@@ -1,7 +1,7 @@
 #pragma once
 #include <vector>
-#include <cstdlib>
-#include <time.h>
+#include<cstdlib>
+#include<time.h>
 using namespace std;
 
 class Bank
@@ -12,23 +12,30 @@ public:
 	void setDeposit(int);
 	void setLoan_times(int);
 	void setInterest(int);
+
 	void stockPriceFlow(); //內部自行rand()
 	void setOwnStock(int, int);
+
 	void saveMoney(int);
 	void withdrawMoney(int);
 	int getDeposit();
 	int getLoan_times();
 	int getInterest();
+
 	vector<float> getStockPrice();
 	vector<int> getOwnStock();
 	vector<int> getUpDownLimit();
+
 private:
 	int deposit;
 	int loan_times;
 	int interest;
-	int roundCount;
+
+	int flowTimes;
 	vector<float> StockPrice;
 	vector<float> minStockPrice, maxStockPrice; // 當回合的漲跌上下限
 	vector<int> ownStock;
 	vector<int> upDownLimit; // 0:未達漲跌停板 1:漲停板 2:跌停板
+	clock_t lastTime;
+	clock_t nowTime;
 };
